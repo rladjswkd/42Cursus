aa921c4390f2ea7d6e350f5d7bd4e4b37bcaeb3d
rladjswkd <dlfmaqkrrudah@gmail.com>
2021-11-22 00:58:39 +0900

    modified: ft_split

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyepark <gyepark@student.42seoul.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 00:58:08 by gyepark           #+#    #+#             */
+/*   Updated: 2021/11/22 00:58:10 by gyepark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 static int	count_words(char const *s)
 {
@@ -19,36 +31,40 @@ static int	count_words(char const *s)
 	return (cnt + (start != end));
 }
 
+static char	**free_assigned(char **output, int to)
+{
+	int	i;
+
+	i = 0;
+	while (i < to)
+		free(output[i]);
+	free(output);
+	return (0);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**output;
-	int		start;
-	int		end;
+	char	*start;
 	int		i;
 
 	output = (char **)malloc(sizeof(char *) * (count_words(s) + 1));
 	if (output == 0)
 		return (0);
-	start = 0;
-	end = -1;
+	start = s;
 	i = 0;
-	while (++end)
+	while (*s)
 	{
-		if (s[end] == c && start < end)
+		if ((*s == c && start < s) || (*s != c && *(s + 1) == 0 && start <= s))
 		{
-			output[i] = (char *)malloc(sizeof(char) * (end - start + 1));
+			output[i] = (char *)malloc(sizeof(char) * (s - start + 1));
 			if (output[i] == 0)
-				return (0);
-			ft_strlcpy(output[i++], s + start, end - start + 1);
-			start = end + 1;
+				return (free_assigned(output, i));
+			ft_strlcpy(output[i++], start, s - start + 1);
+			start = ++s;
 		}
-	}
-	if (start != end)
-	{
-		output[i] = (char *)malloc(sizeof(char) * (end - start + 1));
-		if (output[i] == 0)
-			return (0);
-		ft_strlcpy(output[i++], s + start, end - start + 1);
+		else
+			s++;
 	}
 	output[i] = 0;
 	return (output);
