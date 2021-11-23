37f49b591f938a82fe040e07e8cf2632dd04ccb3
rladjswkd <dlfmaqkrrudah@gmail.com>
2021-11-23 23:14:13 +0900

    modified: ft_strnstr

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: gyepark <gyepark@student.42seoul.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 23:54:14 by gyepark           #+#    #+#             */
-/*   Updated: 2021/11/23 16:38:55 by gyepark          ###   ########.fr       */
+/*   Updated: 2021/11/23 23:13:18 by gyepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	if (len < len_needle)
 		return ((char *)0);
 	i = -1;
-	while (++i <= len - len_needle)
+	while (++i <= len - len_needle && haystack[i])
 		if (!ft_strncmp(haystack + i, needle, len_needle))
 			return ((char *)(haystack + i));
 	return (0);
