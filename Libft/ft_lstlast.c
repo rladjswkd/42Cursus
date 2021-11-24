4fcc5d1dbc41147de570f66d919094ce3f51f847
rladjswkd <dlfmaqkrrudah@gmail.com>
2021-11-25 01:41:58 +0900

    modified: ft_lstlast.c

--- Libft/ft_lstlast.c
+++ Libft/ft_lstlast.c
@@ -5,14 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gyepark <gyepark@student.42seoul.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/25 01:36:10 by gyepark           #+#    #+#             */
-/*   Updated: 2021/11/25 01:36:11 by gyepark          ###   ########.fr       */
+/*   Created: 2021/11/24 19:53:08 by gyepark           #+#    #+#             */
+/*   Updated: 2021/11/24 19:53:09 by gyepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 t_list	*ft_lstlast(t_list *lst)
 {
+	if (!lst)
+		return ((t_list *)lst);
 	while (lst->next)
 		lst = lst->next;
 	return (lst);
