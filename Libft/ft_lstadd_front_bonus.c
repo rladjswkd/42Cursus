14da04c2e50e6ae6e28c55ee67e2b089924950ca
rladjswkd <dlfmaqkrrudah@gmail.com>
2021-11-24 16:05:09 +0900

    deleted: libft_bonus, modified: ft_lstnew_bonus, added: ft_lstadd_front_bonus

--- Libft/ft_lstadd_front_bonus.c
+++ Libft/ft_lstadd_front_bonus.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gyepark <gyepark@student.42seoul.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/24 16:03:30 by gyepark           #+#    #+#             */
-/*   Updated: 2021/11/24 16:03:31 by gyepark          ###   ########.fr       */
+/*   Created: 2021/11/24 16:03:23 by gyepark           #+#    #+#             */
+/*   Updated: 2021/11/24 16:03:24 by gyepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-t_list	*ft_lstnew(void *content)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	*node;
-
-	node = (t_list *)malloc(sizeof(t_list));
-	if (node == 0)
-		return ((t_list *)0);
-	node->content = content;
-	node->next = (t_list *)0;
-	return (node);
+	*(new->next) = **lst;
+	*lst = new;
 }
