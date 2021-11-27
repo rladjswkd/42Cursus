dacd5408ed04e8da949e9361dca53f55f623ffa9
rladjswkd <dlfmaqkrrudah@gmail.com>
2021-11-27 21:41:07 +0900

    modified: ft_strlcat

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gyepark <gyepark@student.42seoul.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 16:28:25 by gyepark           #+#    #+#             */
-/*   Updated: 2021/11/27 19:30:00 by gyepark          ###   ########.fr       */
+/*   Updated: 2021/11/22 16:28:26 by gyepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	len_src;
 	size_t	i;
 
+	len_dest = ft_strlen(dst);
 	len_src = ft_strlen(src);
-	if (dstsize == 0 || dst == 0)
+	if (dstsize == 0 || dstsize <= len_dest)
 		return (dstsize + len_src);
 	i = 0;
-	len_dest = ft_strlen(dst);
 	while (len_dest + i + 1 < dstsize && src[i])
 	{
 		dst[len_dest + i] = src[i];
