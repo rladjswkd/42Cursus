00f7be53c2314fe3cb1b9a0a2b0e8cfacf5914db
rladjswkd <dlfmaqkrrudah@gmail.com>
2021-11-29 14:02:59 +0900

    modified: ft_memmove

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gyepark <gyepark@student.42seoul.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 20:41:48 by gyepark           #+#    #+#             */
-/*   Updated: 2021/11/23 19:11:34 by gyepark          ###   ########.fr       */
+/*   Updated: 2021/11/29 14:02:42 by gyepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t			i;
-	unsigned char	*dst_char;
-	unsigned char	*src_char;
+	unsigned char	*dst_temp;
+	unsigned char	*src_temp;
 
 	if (dst == 0 && src == 0)
 		return (dst);
-	dst_char = (unsigned char *)dst;
-	src_char = (unsigned char *)src;
+	dst_temp = (unsigned char *)dst;
+	src_temp = (unsigned char *)src;
 	if (dst < src)
 	{
 		i = -1;
 		while (++i < len)
-			dst_char[i] = src_char[i];
+			dst_temp[i] = src_temp[i];
 	}
 	else
 	{
 		while (len--)
-			dst_char[len] = src_char[len];
+			dst_temp[len] = src_temp[len];
 	}
 	return ((void *)dst);
 }
