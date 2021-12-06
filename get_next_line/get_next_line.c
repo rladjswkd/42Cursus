281ee9e24370c1ed8794b79b1fea07ef71cba99d
rladjswkd <dlfmaqkrrudah@gmail.com>
2021-12-06 18:57:28 +0900

    modified: get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,28 +6,37 @@
 /*   By: gyepark <gyepark@student.42seoul.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 23:49:15 by gyepark           #+#    #+#             */
-/*   Updated: 2021/12/05 22:54:51 by gyepark          ###   ########.fr       */
+/*   Updated: 2021/12/06 18:56:23 by gyepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-static char	*read_file(int fd, t_builder *builder)
+static char	*free_builder_data(t_builder *builder)
+{
+	free(builder->data);
+	builder->data = 0;
+	return (0);
+}
+
+static int	read_file(int fd, t_builder *builder)
 {
 	int		count;
 	char	buffer[BUFFER_SIZE];
+	char	*temp_data;
 
 	while (builder->index == -1)
 	{
 		count = read(fd, buffer, BUFFER_SIZE);
 		if (count < 1)
-			return (0);
-		builder->data = concat_data(builder->data, buffer, builder->len, count);
-		if (!(builder->data))
-			return (0);
+			return (count);
+		temp_data = concat_data(builder->data, buffer, builder->len, count);
+		if (!temp_data)
+			return (-1);
+		builder->data = temp_data;
 		builder->index = find_new_line(builder->data, builder->len, count);
 		builder->len += count;
 	}
-	return (builder->data);
+	return (1);
 }
 
 static char	*build_line(t_builder *builder)
@@ -38,31 +47,34 @@ static char	*build_line(t_builder *builder)
 	if (builder->index == -1)
 	{
 		line = (char *)malloc(sizeof(char) * (builder->len + 1));
+		if (!line)
+			return (0);
 		copy_data(line, builder->data, builder->len + 1, 1);
-		free(builder->data);
-		builder->data = 0;
 		return (line);
 	}
-	temp_data = builder->data;
 	line = get_part(builder->data, 0, builder->index + 1, 1);
-	builder->data = get_part(builder->data,
+	temp_data = get_part(builder->data,
 			builder->index + 1, builder->len - (builder->index + 1), 0);
-	if (!line || !(builder->data))
+	if (!line || !temp_data)
 		return (0);
+	free(builder->data);
+	builder->data = temp_data;
 	builder->len -= (builder->index + 1);
 	builder->index = find_new_line(builder->data, 0, builder->len);
-	if (builder->len == 0)
-		free(builder->data);
-	free(temp_data);
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
 	static t_builder	builder = {0, 0, -1};
+	int					read_status;
 
-	if (fd < 0 || BUFFER_SIZE < 1
-		|| (!read_file(fd, &builder) && !(builder.data)))
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (0);
+	read_status = read_file(fd, &builder);
+	if (read_status == -1)
+		return (free_builder_data(&builder));
+	else if (read_status == 0)
 		return (0);
 	return (build_line(&builder));
 }
