f5004864ad890f376be967eaf701a5f5dfe5eb18
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-02-21 19:53:11 +0900

    modified: minitalk.h header protection

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,11 +6,12 @@
 /*   By: gyepark <gyepark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 21:00:05 by gyepark           #+#    #+#             */
-/*   Updated: 2022/02/21 11:23:43 by gyepark          ###   ########.fr       */
+/*   Updated: 2022/02/21 19:52:38 by gyepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
+# define MINITALK_H
 # include <sys/types.h>
 # include <unistd.h>
 # include <signal.h>
