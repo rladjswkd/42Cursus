94ece2051f90e3aa9b5b141c6221439f4cc4db02
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-03-14 22:02:26 +0900

    modified: test.c

--- fract-ol/test.c
+++ fract-ol/test.c
@@ -5,9 +5,9 @@
 #define WIDTH		1000
 #define HEIGHT		1000
 #define ESCAPE_RADIUS	2
-#define MAX_ITERATION	256
-#define C_RE		(double)-0.4
-#define C_IM		(double)0.6
+#define MAX_ITERATION	240
+#define C_RE		0.285	
+#define C_IM		0.01
 
 typedef struct	s_complex
 {
@@ -31,40 +31,47 @@ typedef struct	s_vars
 	t_data		data;
 }		t_vars;
 
-int	is_in_julia_set(int x, int y)
-{
-	t_complex	z;
-	int		r_squared;
-	int		iteration;
-	int		size;
-	double		temp;
-
-	size = WIDTH;
-	if (HEIGHT < WIDTH)
-		size = HEIGHT;
-	z.re = (x - WIDTH / 2.0) * (ESCAPE_RADIUS + ESCAPE_RADIUS) / size;
-	z.im = (HEIGHT / 2.0 - y) * (ESCAPE_RADIUS + ESCAPE_RADIUS) / size;
-	r_squared = pow(ESCAPE_RADIUS, 2);
-	iteration = 0;
-	while (iteration < MAX_ITERATION && pow(z.re, 2) + pow(z.im, 2) < r_squared)
-	{
-		temp = z.re;
-		z.re = pow(z.re, 2) - pow(z.im, 2) + C_RE;
-		z.im = 2 * temp * z.im + C_IM;
-		iteration++;
-	}
-	if (iteration == MAX_ITERATION)
-		return (0x00000000);
-	else
-		return (iteration << 21 | iteration << 10 | iteration << 8);
-}
-
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
+}
+
+int	get_rgb(int r, int g, int b)
+{
+	return (r << 16 | g << 8 | b);
+}
+
+int	is_in_julia_set(int x, int y)
+{
+	t_complex	z;
+	int		r_squared;
+	int		iter;
+	int		size;
+	double		temp;
+	size = WIDTH;
+	if (HEIGHT < WIDTH)
+		size = HEIGHT;
+	z.re = (x - WIDTH / 2.0) * (ESCAPE_RADIUS + ESCAPE_RADIUS) / size;
+	z.im = (HEIGHT / 2.0 - y) * (ESCAPE_RADIUS + ESCAPE_RADIUS) / size;
+	r_squared = pow(ESCAPE_RADIUS, 2);
+	iter = 0;
+	while (pow(z.re, 2) + pow(z.im, 2) <= r_squared && iter++ < MAX_ITERATION)
+	{
+		temp = z.re;
+		z.re = pow(z.re, 2) - pow(z.im, 2) + C_RE;
+		z.im = 2 * temp * z.im + C_IM;
+	}
+	if (iter > MAX_ITERATION)
+		return (0);
+	else
+	{
+		iter = (int)((double)iter / MAX_ITERATION * 255.0);
+		iter = iter;
+		return (iter << 16 | iter << 8 | iter);
+	}
 }
 
 int	key_pressed(int keycode, t_vars *vars)
@@ -85,10 +92,12 @@ int	key_pressed(int keycode, t_vars *vars)
 	return (0);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	t_vars	vars;
 
+	(void)argc;
+	(void)argv;
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, "fractol");
 	vars.data.img = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
