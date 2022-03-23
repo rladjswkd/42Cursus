e94e22249e4f4f8acac8dcbe7ae23c305d9c06ce
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-03-23 09:01:12 +0900

    in progress

--- fract-ol/test.c
+++ fract-ol/test.c
@@ -5,12 +5,11 @@
 #include <stdio.h>
 #define EVENT_KEY		114//15
 #define ESC_KEY			65307//53
+#define WHEELUP			4
+#define WHEELDOWN		5
 #define WIDTH			1000
 #define HEIGHT			1000
-#define ESCAPE_RADIUS	2
-#define MAX_ITERATION	32//256//48//64//100//120
-#define C_RE			-0.4//0.45//0//-0.835//-0.70716	
-#define C_IM			0.6//0.1428//-0.8//-0.2321//-0.3842
+#define SIZE			1000
 
 typedef struct	s_complex
 {
@@ -35,12 +34,23 @@ typedef struct	s_params
 	t_complex	julia_c;
 }		t_params;
 
+typedef	struct	s_screen
+{
+	double	zoom_rate;
+	int		x_start;
+	int		y_start;
+	int		img_width;
+	int		img_height;
+}		t_screen;
+
 typedef struct	s_vars
 {
 	void	*mlx;
 	void	*win;
 	t_img		img;
 	t_params	params;
+	t_screen	scr;
+	int		effect_val;
 }		t_vars;
 
 void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
@@ -50,7 +60,7 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
-
+// 안씀.
 int	get_rgb(int r, int g, int b)
 {
 	return (r << 16 | g << 8 | b);
@@ -66,29 +76,42 @@ int	get_len(char *str)
 	return (len);
 }
 
+int	get_sign(char **str)
+{
+       	int	sign;
+
+	sign = 1;
+	if (**str == '-' || **str == '+')
+		if (*((*str)++) == '-')
+			sign = -1;
+	return (sign);
+}
+
 double	*get_double(char *str, double *val)
 {
+	long double	temp;
 	double		scale;
        	int		sign;
 
-	sign = 1;
-	if (*str == '-' || *str == '+')
-		if (*(str++) == '-')
-			sign = -1;
-	*val = 0;
+	sign = get_sign(&str);
+	temp = 0;
 	while (47 < *str && *str < 58)
-		*val = *val * 10 + sign * (*(str++) - 48);
+		temp = temp * 10 + sign * (*(str++) - 48);
 	if (*str != '.' && *str != '\0')
 		return (0);
+	*val = (double)temp;
+	if (*str == '\0')
+		return (val);
 	str++;
 	scale = 10;
 	while (47 < *str && *str < 58)
 	{
-		*val += sign *(*(str++) - 48) / scale;
+		temp += sign * (*(str++) - 48) / scale;
 		scale *= 10;
 	}
 	if (*str != '\0')
 		return (0);
+	*val = (double)temp;
 	return (val);
 }
 
@@ -96,10 +119,7 @@ int	*get_int(char *str, int *val)
 {
 	int	sign;
 
-	sign = 1;
-	if (*str == '-' || *str == '+')
-		if (*(str++) == '-')
-			sign = -1;
+	sign = get_sign(&str);
 	*val = 0;
 	while (47 < *str && *str < 58)
 		*val = *val * 10 + sign * (*(str++) - 48);
@@ -107,7 +127,6 @@ int	*get_int(char *str, int *val)
 		return (0);
 	return (val);
 }
-
 /*
 int	check_julia_set(int x, int y)
 {
@@ -137,38 +156,6 @@ int	check_julia_set(int x, int y)
 		iter = (int)((double)iter / max_iteration * 255.0);
 		iter = 255 - iter;
 		return (iter << 16 | iter << 8 |  iter);
-	}
-}
-*/
-
-int	check_julia_set(int x, int y, t_vars *vars)
-{
-	t_complex	z;
-	int		r_squared;
-	int		iter;
-	int		size;
-	double		temp;
-	
-	size = WIDTH;
-	if (HEIGHT < WIDTH)
-		size = HEIGHT;
-	z.re = (x - WIDTH / 2.0) * (vars->params.radius + vars->params.radius) / size;
-	z.im = (HEIGHT / 2.0 - y) * (vars->params.radius + vars->params.radius) / size;
-	r_squared = vars->params.radius * vars->params.radius;
-	iter = 0;
-	while (z.re * z.re + z.im * z.im <= r_squared && iter++ < vars->params.max_iter)
-	{
-		temp = z.re;
-		z.re = z.re * z.re - z.im * z.im + vars->params.julia_c.re;
-		z.im = 2 * temp * z.im + vars->params.julia_c.im;
-	}
-	if (iter > vars->params.max_iter)
-		return (0);
-	else
-	{
-		iter = (int)((double)iter / vars->params.max_iter * 255.0);
-		iter = 255 - iter;
-		return (iter << 16 | iter << 8 | iter);
 	}
 }
 
@@ -205,19 +192,88 @@ int	check_mandelbrot_set(int x, int y)
 		return (iter << 16 | iter << 8 |  iter);
 	}
 }
+*/
 
+t_complex	get_transposed_point(int x, int y, t_vars *vars)
+{
+	t_complex	res;
+
+	res.re = (x - WIDTH / 2.0) * (vars->params.radius + vars->params.radius) / SIZE;
+	res.im = (HEIGHT / 2.0 - y) * (vars->params.radius + vars->params.radius) / SIZE;
+	return (res);
+}
+
+int	check_divergence(t_complex z, t_complex c, int squared, int max_iter)
+{
+	int		iter;
+	double		temp;
+
+	iter = 0;
+	while (z.re * z.re + z.im * z.im <= squared && iter++ < max_iter)
+	{
+		temp = z.re;
+		z.re = z.re * z.re - z.im * z.im + c.re;
+		z.im = 2 * temp * z.im + c.im;
+	}
+	return (iter);
+}
+
+int	get_iter_color(int iter, int max_iter)
+{
+	if (iter == max_iter)
+		return (0);
+	iter = (int)((double)iter / max_iter * 255.0);
+	iter = 255 - iter;
+	return (iter << 16 | iter << 8 | iter);
+}
+
+int	check_julia_set(int x, int y, t_vars *vars)
+{
+	t_complex	z;
+	t_complex	c;
+	int		squared;
+	int		iter;
+	
+	z = get_transposed_point(x, y, vars);
+	c = vars->params.julia_c;
+	squared = vars->params.radius * vars->params.radius;
+	iter = check_divergence(z, c, squared, vars->params.max_iter);
+	return (get_iter_color(iter, vars->params.max_iter));
+}
+
+int	check_mandelbrot_set(int x, int y, t_vars *vars)
+{
+	t_complex	z;
+	t_complex	c;
+	int		squared;
+	int		iter;
+	
+	z.re = 0;
+	z.im = 0;
+	c = get_transposed_point(x, y, vars);
+	squared = vars->params.radius * vars->params.radius;
+	iter = check_divergence(z, c, squared, vars->params.max_iter);
+	return (get_iter_color(iter, vars->params.max_iter));
+}
 void	exit_print_param_info(t_vars *vars)
 {
-	static char	*str = "\nParameter format:\n\t[fractal type][escape radius][max iteration][real value][imaginary value]\n\nFractal type is integer:\n\t1) Julia set, 2) Mandelbrot set, 3) Newton fractal\n\nEscape radius and max iteration are integers.\n\nLast two parameters are floating point numbers and for Julia set.\n\n";
+	char	*str;
 
-	if (write(2, str, get_len(str))) 
+	str = "\nParameter format:\n\t[fractal type][escape radius][max iteration][real value][imaginary value]\n\nFractal type is integer:\n\t1) Julia set, 2) Mandelbrot set, 3) Newton fractal\n\nEscape radius and max iteration are integers.\n\nLast two parameters are floating point numbers and for Julia set.\n\n";
+
+	if (write(2, str, get_len(str))) // remove if statements on mac
+	{}
+	mlx_destroy_image(vars->mlx, vars->img.ptr);
 	mlx_destroy_window(vars->mlx, vars->win);
+	free(vars->mlx);
 	exit(EXIT_FAILURE);
 }
 
 void	exit_complete(t_vars *vars)
 {
+	mlx_destroy_image(vars->mlx, vars->img.ptr);
 	mlx_destroy_window(vars->mlx, vars->win);
+	free(vars->mlx);
 	exit(EXIT_SUCCESS);
 }
 
@@ -225,11 +281,14 @@ t_params	*process_params(int argc, char **argv, t_params *params)
 {
 	if (argc < 4)
 		return (0);
-	if (!get_int(argv[1], &(params->type)) || !(0 < params->type && params->type < 4))
+	if (!get_int(argv[1], &(params->type))
+			|| !(0 < params->type && params->type < 4))
 		return (0);
 	if (!get_int(argv[2], &(params->radius)))
 		return (0);
 	if (!get_int(argv[3], &(params->max_iter)))
+		return (0);
+	if (params->type == 1 && argc < 6)
 		return (0);
 	if (params->type == 1 && !get_double(argv[4], &(params->julia_c.re)))
 		return (0);
@@ -246,14 +305,16 @@ void	draw_fractal(t_vars *vars)
 
 	if (vars->params.type == 1)
 		fp = &check_julia_set;
+	else if (vars->params.type == 2)
+		fp = &check_mandelbrot_set;
+	else
+		fp = 0;
 	x_idx = -1;
 	while (++x_idx < WIDTH)
 	{
 		y_idx = -1;
 		while (++y_idx < HEIGHT)
-			my_mlx_pixel_put(&(vars->img), x_idx, y_idx, (*fp)(x_idx, y_idx, vars));
-			//my_mlx_pixel_put(&(vars->data), x_idx, y_idx, check_julia_set(x_idx, y_idx));
-			//my_mlx_pixel_put(&(vars->img), x_idx, y_idx, check_mandelbrot_set(x_idx, y_idx));
+			my_mlx_pixel_put(&(vars->img), x_idx, y_idx, (*fp)(vars->scr.x_start + x_idx, vars->scr.y_start + y_idx, vars));
 	}
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->img.ptr, 0, 0);
 }
@@ -262,7 +323,40 @@ int	key_press_handler(int keycode, t_vars *vars)
 {
 	if (keycode == ESC_KEY)
 		exit_complete(vars);
+	return (0);
+}
+
+int	base_handler(t_vars *vars)
+{
+	// for psychedelic effects.
+	//vars->effect_val
 	draw_fractal(vars);
+	return (0);
+}
+
+void	zoom_in(int x, int y, t_vars *vars)
+{
+//	vars->x_start = (int)((double)x / WIDTH * (WIDTH * vars->zoom_rate)) - x;
+//	vars->y_start = (int)((double)y / HEIGHT * (HEIGHT * vars->zoom_rate)) - y;
+	vars->scr.x_start = (int)((double)x / vars->scr.img_width * (vars->scr.img_width * vars->scr.zoom_rate)) - x;
+	vars->scr.y_start = (int)((double)y / vars->scr.img_height * (vars->scr.img_height * vars->scr.zoom_rate)) - y;
+	vars->scr.img_width *= vars->scr.zoom_rate;
+	vars->scr.img_height *= vars->scr.zoom_rate;
+}
+
+void	zoom_out(int x, int y, t_vars *vars)
+{
+	(void)x;
+	(void)y;
+	(void)vars;
+}
+
+int	zoom_handler(int button, int x, int y, t_vars *vars)
+{
+	if (button == WHEELUP)
+		zoom_in(x, y, vars);
+	else if (button == WHEELDOWN)
+		zoom_out(x, y, vars);
 	return (0);
 }
 
@@ -276,7 +370,15 @@ int	main(int argc, char **argv)
 	vars.img.addr = mlx_get_data_addr(vars.img.ptr, &vars.img.bits_per_pixel, &vars.img.line_length, &vars.img.endian);
 	if (!process_params(argc, argv, &(vars.params)))
 		exit_print_param_info(&vars);
+	vars.scr.zoom_rate = 1.1;
+	vars.scr.x_start = 0;
+	vars.scr.y_start = 0;
+	vars.scr.img_width = WIDTH;
+	vars.scr.img_height = HEIGHT;
+	//vars.effect_val = 0;
 	mlx_hook(vars.win, 2, 1L<<0, key_press_handler, &vars);
+	mlx_hook(vars.win, 4, 1L<<2, zoom_handler, &vars);
+	mlx_loop_hook(vars.mlx, base_handler, &vars);
 	mlx_loop(vars.mlx);
 	return (0);
 }
--- fract-ol/tests/ft_atod_test.c
+++ fract-ol/tests/ft_atod_test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
-
+/*
 double	ft_atod(char *str)
 {
 	long double	integer;
@@ -35,14 +35,77 @@ double	ft_atod(char *str)
 	}
 	return ((double)integer);
 }
+*/
+
+
+int	get_sign(char **str)
+{
+       	int	sign;
+
+	sign = 1;
+	if (**str == '-' || **str == '+')
+		if (*((*str)++) == '-')
+			sign = -1;
+	return (sign);
+}
+
+double	*get_double(char *str, double *val)
+{
+	long double	temp;
+	double		scale;
+       	int		sign;
+
+	sign = get_sign(&str);
+	temp = 0;
+	while (47 < *str && *str < 58)
+		temp = temp * 10 + sign * (*(str++) - 48);
+	if (*str != '.' && *str != '\0')
+		return (0);
+	*val = (double)temp;
+	if (*str == '\0')
+		return (val);
+	str++;
+	scale = 10;
+	while (47 < *str && *str < 58)
+	{
+		temp += sign * (*(str++) - 48) / scale;
+		scale *= 10;
+	}
+	if (*str != '\0')
+		return (0);
+	*val = (double)temp;
+	return (val);
+}
+
+int	*get_int(char *str, int *val)
+{
+	int	sign;
+
+	sign = get_sign(&str);
+	*val = 0;
+	while (47 < *str && *str < 58)
+		*val = *val * 10 + sign * (*(str++) - 48);
+	if (*str != '\0')
+		return (0);
+	return (val);
+}
 int	main(void)
 {
+	double	val;
+
 	printf("%lf\n" , DBL_MIN);
-	printf("%lf\n", ft_atod("0.0"));
+	get_double("0.0", &val);
+	printf("%lf\n", val);
+
 	printf("%lf\n", 123.456);
-	printf("%lf\n", ft_atod("123.456"));
+	get_double("123.456", &val);
+	printf("%lf\n", val);
+
 	printf("%lf\n" , DBL_MAX);
-	printf("%lf\n", ft_atod("179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.000000"));
+	get_double("179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.000000", &val);
+	printf("%lf\n", val);
+
 	printf("%lf\n", -123.456);
-	printf("%lf\n", ft_atod("-123.456"));
+	get_double("-123.456", &val);
+	printf("%lf\n", val);
 }
