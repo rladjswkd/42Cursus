11a263546c181040f4e2dc079462cb01bd0faa3e
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-09-08 02:54:43 +0900

    in progress: cpp module 00 ex01

---
+++ cpp/module00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyepark <gyepark@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 01:44:55 by gyepark           #+#    #+#             */
+/*   Updated: 2022/09/08 01:44:59 by gyepark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+void	print_uppercase(char *input)
+{
+	while (*input)
+		std::cout << (unsigned char)(std::toupper(*(input++)));
+}
+
+int	main(int argc, char **argv)
+{
+	int	i = 0;
+
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		while (++i < argc)
+			print_uppercase(argv[i]);
+	std::cout << std::endl;
+	return (0);
+}
