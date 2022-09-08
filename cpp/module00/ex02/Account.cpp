688d35f65bf5ea08a6fd7786c10ea9ff3302386b
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-09-08 21:59:38 +0900

    in progress: cpp 00 ex02

---
+++ cpp/module00/ex02/Account.cpp
@@ -0,0 +1,53 @@
+#include "Account.hpp"
+
+static int	getNbAccounts(void){
+
+}
+
+static int	getTotalAmount(void){
+
+}
+
+static int	getNbDeposits(void){
+
+}
+
+static int	getNbWithdrawals(void){
+
+}
+
+static void	displayAccountsInfos(void){
+    
+}
+
+Account::Account(int initial_deposit){
+    
+}
+
+Account::~Account(void){
+
+}
+
+void	Account::makeDeposit(int deposit){
+
+}
+
+bool	Account::makeWithdrawal(int withdrawal){
+
+}
+
+int		Account::checkAmount(void) const{
+
+}
+
+void	Account::displayStatus(void) const{
+
+}
+
+Account::Account(void){
+
+}
+
+static void	_displayTimestamp(void){
+
+}
