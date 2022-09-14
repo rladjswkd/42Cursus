8b2913d091fbc55e2149bc16c499854bc9223620
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-09-14 21:56:43 +0900

    in progress: module05

---
+++ cpp/module05/ex01/Form.hpp
@@ -0,0 +1,27 @@
+class Form
+{
+private:
+
+public:
+
+	Form(void);
+	Form(Form &);
+	~Form(void);
+	Form	&operator=(Form &);
+};
+
+Form::Form(void){
+
+}
+
+Form::Form(Form &){
+
+}
+
+Form::~Form(void){
+
+}
+
+Form	&Form::operator=(Form &){
+
+}
