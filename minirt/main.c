3775768dab48b72bacd08db3cf1e2dd0fd8e35c7
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-09-28 21:47:46 +0900

    backup

--- minirt/main.c
+++ minirt/main.c
@@ -3,9 +3,17 @@
 #include "get_next_line.h"
 int	main(void)
 {
-	int	fd = open("minimalist.rt", O_RDONLY);
+	int		fd;
 	char	*p;
-	while ((p = get_next_line(fd)) > 0)
-		printf("%s\n", p);
+
+	fd = open("minimalist.rt", O_RDONLY);
+	if (fd < 0)
+		return (0); // 에러 문자열 출력하고 처리해주기
+	while (1) // 개행문자만 나오면 넘기기
+	{
+		p = get_next_line(fd);
+		if (p[0] == '\n')
+			continue ;
+	}
 	return (0);
 }
