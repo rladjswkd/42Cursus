1a21c2dd677cf492f9334f53f425ed27c7a3cbe9
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-11-21 00:21:50 +0900

    backup

---
+++ minirt/bonus/constant_bonus.h
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   constant_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyepark <gyepark@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 19:57:50 by gyepark           #+#    #+#             */
+/*   Updated: 2022/11/16 16:16:31 by gyepark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONSTANT_BONUS_H
+# define CONSTANT_BONUS_H
+
+# define ERROR_MSG			"Error\nSomething went wrong!\n"
+
+# define NONE				0
+# define SPHERE				1
+# define CYLINDER			2
+# define PLANE				3
+# define CONE				4
+# define CAMERA				5
+# define LIGHT				6
+
+# define SHADOW_BIAS 		1.00001
+
+# define BUMP_HIGHT_WEIGHT	100
+# define COLOR_IMG			"bump.xpm"
+# define BUMP_IMG			"bump3.xpm"
+
+# define CHECKER			1
+# define IMAGE				2
+# define BUMP				4
+
+# define WIDTH 				1280
+# define HEIGHT				720
+
+# define THREAD				12
+
+# define ANGLE				10
+
+# define MOUSE_LEFT			1
+# define W					119
+# define A					97
+# define S					115
+# define D					100
+# define Q					113
+# define E					101
+# define ONE				49
+# define TWO				50
+# define THREE				51
+# define FOUR				52
+# define C					8
+# define B					11
+# define I					34
+# define K					40
+# define L					37
+# define UP					126
+# define DOWN				125
+# define LEFT				123
+# define RIGHT				124
+# define ESC				65307
+
+# define S_EXP				32
+#endif
