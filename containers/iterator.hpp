5f31de9e034b341b869cb8bfbdc0581134f511ae
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-11-27 15:25:32 +0900

    feat: implement iterator_traits including pointer, const pointer specialization

---
+++ containers/iterator.hpp
@@ -0,0 +1,26 @@
+template <typename Iter>
+struct iterator_traits {
+	typedef typename Iter::iterator_category	iterator_category;
+	typedef typename Iter::value_type			value_type;
+	typedef typename Iter::difference_type		difference_type;
+	typedef typename Iter::pointer				pointer;
+	typedef typename Iter::reference			reference;	
+};
+
+template <typename T>
+struct iterator_traits<T*> {
+	typedef std::ptrdiff_t						difference_type;
+	typedef T									value_type;
+	typedef T*									pointer;
+	typedef T&									reference;
+	typedef std::random_access_iterator_tag		iterator_category;
+};
+
+template <typename T>
+struct iterator_traits<const T*> {
+	typedef std::ptrdiff_t						difference_type;
+	typedef T									value_type;
+	typedef const T*							pointer;
+	typedef const T&							reference;
+	typedef std::random_access_iterator_tag		iterator_category;
+};
