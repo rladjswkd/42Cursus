dd5b2bde43c85acf556e40d55c150eede576a603
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-12-06 18:13:04 +0900

    feat: make namespace ft

--- containers/enable_if.hpp
+++ containers/enable_if.hpp
@@ -1,7 +1,9 @@
-template <bool B, class T = void>
-struct enable_if {};
+namespace ft {
+	template <bool B, class T = void>
+	struct enable_if {};
 
-template <class T>
-struct enable_if<true, T> {
-	typedef T type;
-};
+	template <class T>
+	struct enable_if<true, T> {
+		typedef T type;
+	};
+}
