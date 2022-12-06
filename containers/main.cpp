082487231c80fe1525f403de722ed114c1034d92
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-12-06 15:07:38 +0900

    test: add more tests for is_integral

--- containers/main.cpp
+++ containers/main.cpp
@@ -2,6 +2,24 @@
 #include <iostream>
 
 int main() {
-	std::cout <<  ft::is_integral<int>::value << std::endl;
+	std::cout << ft::is_integral<int>() << std::endl;
+	std::cout << ft::is_integral<short>::value << std::endl;
+	std::cout << ft::is_integral<bool>::value << std::endl;
+	std::cout << ft::is_integral<signed char>::value << std::endl;
+	std::cout << ft::is_integral<unsigned char>::value << std::endl;
+	std::cout << ft::is_integral<wchar_t>::value << std::endl;
+	std::cout << ft::is_integral<char16_t>::value << std::endl;
+	std::cout << ft::is_integral<char32_t>::value << std::endl;
+	std::cout << ft::is_integral<short>::value << std::endl;
+	std::cout << ft::is_integral<unsigned short>::value << std::endl;
+	std::cout << ft::is_integral<int>::value << std::endl;
+	std::cout << ft::is_integral<unsigned int>::value << std::endl;
+	std::cout << ft::is_integral<long>::value << std::endl;
+	std::cout << ft::is_integral<unsigned long>::value << std::endl;
+	std::cout << ft::is_integral<long long>::value << std::endl;
+	std::cout << ft::is_integral<unsigned long long>::value << std::endl;
+	std::cout << ft::is_integral<__int128_t>::value << std::endl;
+	std::cout << ft::is_integral<__uint128_t>::value << std::endl;
+	std::cout << ft::is_integral<std::string>::value << std::endl;
 	return (0);
 }
