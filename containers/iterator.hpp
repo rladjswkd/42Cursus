1938a6cee1b022c41dd7403e6b4684684156a8b1
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-12-09 13:19:29 +0900

    style: remove unnecessary white spaces

--- containers/iterator.hpp
+++ containers/iterator.hpp
@@ -3,10 +3,10 @@
 # include <stddef.h>
 # include <iterator>
 namespace ft {
-	struct input_iterator_tag  {};
+	struct input_iterator_tag {};
 	struct output_iterator_tag {};
-	struct forward_iterator_tag       : public input_iterator_tag         {};
-	struct bidirectional_iterator_tag : public forward_iterator_tag       {};
+	struct forward_iterator_tag : public input_iterator_tag {};
+	struct bidirectional_iterator_tag : public forward_iterator_tag {};
 	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
 	template <typename Iter>
