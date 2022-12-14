15813bf89eaeb74384b955c4f97f97d438394e30
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-12-14 21:51:21 +0900

    fix(lexicographical_compare): replace parameters with dereferenced paramters

--- containers/lexicographical_compare.hpp
+++ containers/lexicographical_compare.hpp
@@ -90,9 +90,9 @@ namespace ft {
 	template <class InputIt1, class InputIt2, class Compare>
 	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Compare comp) {
 		for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
-			if (comp(first1, first2))
+			if (comp(*first1, *first2))
 				return (true);
-			if (comp(first2, first1))
+			if (comp(*first2, *first1))
 				return (false);
 		}
 		return (first1 == last1 && first2 != last2);
