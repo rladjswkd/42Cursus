aa7a803e91a1430fbf91abd9f8fd91b0f5039f7b
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-12-18 17:11:32 +0900

    feat: implement pair's relational operators

--- containers/pair.hpp
+++ containers/pair.hpp
@@ -42,6 +42,42 @@ namespace ft {
 	}
 
 	template <class T1, class T2>
+	bool operator==(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs) {
+		return (lhs.first == rhs.first && lhs.second == rhs.second);
+	}
+	
+	template <class T1, class T2>
+	bool operator!=(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs) {
+		return (!(lhs == rhs));
+	}
+
+	template <class T1, class T2>
+	bool operator<(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs) {
+		return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second));
+	}
+
+	template <class T1, class T2>
+	bool operator<=(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs) {
+		return (!(rhs < lhs));
+	}
+
+	template <class T1, class T2>
+	bool operator>(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs) {
+		return (rhs < lhs);
+	}
+
+	template <class T1, class T2>
+	bool operator>=(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs) {
+		return (!(lhs < rhs));
+	}	
+	
+	
+	
+	
+	
+	
+	
+	template <class T1, class T2>
 	inline pair<T1, T2> make_pair(T1 t, T2 u) {
 		return (pair<T1, T2>(t, u));
 	}
