befcd0eaac4ff22e477ab4507b8c3d4dfd09d169
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-12-18 17:12:05 +0900

    test: test pair's relational operators

--- containers/main.cpp
+++ containers/main.cpp
@@ -1,17 +1,19 @@
-// pair::operator= example
+// pair relational operators
 #include "pair.hpp"
-#include <utility>      // std::pair, std::make_pair
-#include <string>       // std::string
+#include <utility>      // std::pair
 #include <iostream>     // std::cout
 
-int main () {
-  ft::pair <std::string,int> planet, homeplanet;
+int main ()
+{
+  ft::pair<int,char> foo (10,'z');
+  ft::pair<int,char> bar (90,'a');
 
-  planet = ft::make_pair("Earth",6371);
+  if (foo==bar) std::cout << "foo and bar are equal\n";
+  if (foo!=bar) std::cout << "foo and bar are not equal\n";
+  if (foo< bar) std::cout << "foo is less than bar\n";
+  if (foo> bar) std::cout << "foo is greater than bar\n";
+  if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+  if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
 
-  homeplanet = planet;
-
-  std::cout << "Home planet: " << homeplanet.first << '\n';
-  std::cout << "Planet size: " << homeplanet.second << '\n';
   return 0;
 }
