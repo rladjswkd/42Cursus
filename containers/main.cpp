c4616ec788674dae84593cfe0e184cbbcbc8540f
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-12-18 16:10:35 +0900

    test: test pair::operator=

--- containers/main.cpp
+++ containers/main.cpp
@@ -1,18 +1,17 @@
-// pair::pair example
+// pair::operator= example
 #include "pair.hpp"
 #include <utility>      // std::pair, std::make_pair
 #include <string>       // std::string
 #include <iostream>     // std::cout
 
 int main () {
-    ft::pair<std::string, double> product1;                     // default constructor
-    ft::pair<std::string, double> product2("tomatoes",2.30);   // value init
-    ft::pair<std::string, double> product3(product2);          // copy constructor
-    ft::pair<std::string, double> product4(ft::make_pair<std::string, double>(std::string("apple"), 4.42));
+  ft::pair <std::string,int> planet, homeplanet;
 
-    std::cout << "The price of " << product2.first << " is $" << product2.second << '\n';
-    std::cout << "The price of " << product3.first << " is $" << product3.second << '\n';
-    std::cout << "The price of " << product4.first << " is $" << product4.second << '\n';
-    
-    return 0;
+  planet = ft::make_pair("Earth",6371);
+
+  homeplanet = planet;
+
+  std::cout << "Home planet: " << homeplanet.first << '\n';
+  std::cout << "Planet size: " << homeplanet.second << '\n';
+  return 0;
 }
