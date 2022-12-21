61420a7bdc0a482f347941a768559ce909b0c362
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-12-21 15:41:43 +0900

    refactor: rename vector_temp.hpp -> vector.hpp and vector.hpp -> vector_old.hpp

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -1,170 +1,104 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
-# include "iterator.hpp"
 # include <memory>
-# include <cstddef> // std::size_t, std::ptrdiff_t
-# include <stdexcept> // exceptions
 
-# include <vector> // remove
-# include <deque>
 namespace ft {
-// vector_base
-	template <class T, class Allocator>
+	template<typename T, typename Allocator>
 	struct vector_base {
-		typedef Allocator	allocator_type;
-		typedef T*			pointer;
-		typedef std::size_t	size_type;
+	public:
 
-		pointer		begin;
-		pointer		end;
-		pointer		capacity_end;
-		Allocator	allocator;
+		typedef Allocator											allocator_type;
+
+	protected:
+
+		// allocator_type is always std::allocator<T> in this project.
+		typedef typename allocator_type::template rebind<T>::other	T_allocator_type;
+		typedef typename allocator_type::pointer					pointer;
+
+		pointer				begin;
+		pointer				end;
+		pointer				end_cap;
+
+		T_allocator_type	t_alloc;
 
 		vector_base();
 		vector_base(const allocator_type& alloc);
-		vector_base(size_type count, const allocator_type& alloc);
+		vector_base(size_t count, const allocator_type& alloc);
 		~vector_base();
 
-		pointer	allocate_memory(size_type count);
-		void	deallocate_memory(pointer ptr, size_type count);
-		void	create_storage(size_type count);
+		pointer	allocate(size_t count);
+		void	deallocate(pointer ptr, size_t count);
+		void	create_storage(size_t count);
+
+		T_allocator_type&		get_T_allocator();
+		const T_allocator_type&	get_T_allocator() const;
+		allocator_type			get_allocator() const;
 	};
 
+
+	template < class T, class Allocator = std::allocator<T> >
+	class vector : protected vector_base<T, Allocator> {
+		
+	};
+///////////////////////////////////////////////////////////////////////
+// vector_base
+///////////////////////////////////////////////////////////////////////
+
+// constructor
 	template <class T, class Allocator>
-	inline vector_base<T, Allocator>::vector_base() : begin(NULL), end(NULL), capacity_end(NULL), allocator() { }
+	inline vector_base<T, Allocator>::vector_base()
+	: begin(NULL), end(NULL), end_cap(NULL), t_alloc() { }
 
 	template <class T, class Allocator>
-	inline vector_base<T, Allocator>::vector_base(const allocator_type& alloc) : begin(NULL), end(NULL), capacity_end(NULL), allocator(alloc) { }
+	inline vector_base<T, Allocator>::vector_base(const allocator_type& alloc)
+	: begin(NULL), end(NULL), end_cap(NULL), t_alloc(alloc) { }
 
 	template <class T, class Allocator>
-	inline vector_base<T, Allocator>::vector_base(size_type count, const allocator_type& alloc) : allocator(alloc) {
-		create_storage(count);
+	inline vector_base<T, Allocator>::vector_base(size_t count, const allocator_type& alloc)
+	: t_alloc(alloc) {
+		create_storage(count)
 	}
 
+// destructor
 	template <class T, class Allocator>
 	inline vector_base<T, Allocator>::~vector_base() {
-		deallocate_memory(begin, apacity_end - begin);
+		deallocate(begin, end_cap - begin);
 	}
 
+// other member functions
 	template <class T, class Allocator>
-	inline T* vector_base<T, Allocator>::allocate_memory(size_type count) {
+	inline vector_base<T, Allocator>::pointer	vector_base<T, Allocator>::allocate(size_t count) {
 		if (count != 0)
-			return (Allocator().allocate(count));  // default constructors are stateless.
+			this->t_alloc.allocate(count);
 		return (pointer());
 	}
 
 	template <class T, class Allocator>
-	inline void vector_base<T, Allocator>::deallocate_memory(pointer ptr, size_type count) {
+	inline void vector_base<T, Allocator>::deallocate(pointer ptr, size_t count) {
 		if (ptr)
-			Allocator().deallocate(ptr, count);
+			this->t_alloc.deallocate(ptr, count);
 	}
 
 	template <class T, class Allocator>
-	inline void vector_base<T, Allocator>::create_storage(size_type count) {
-		begin = allocate_memory(count);
-		end = begin;
-		capacity_end = begin + count;
-	}
-
-
-// vector
-	template < class T, class Allocator = std::allocator<T> >
-	class vector : protected vector_base<T, Allocator> {
-	public:
-	// member types
-		typedef T										value_type;
-		typedef Allocator								allocator_type;
-		typedef std::size_t								size_type;
-		typedef std::ptrdiff_t							difference_type;
-		typedef T&										reference;
-		typedef const T&								const_reference;
-		typedef T*										pointer;
-		typedef const T*								const_pointer;
-		typedef T*										iterator;	// implement iterator later.
-		typedef const T*								const_iterator;
-		typedef ft::reverse_iterator<iterator>			reverse_iterator;
-		typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
-
-	// constructor
-		vector();
-		explicit vector(const Allocator& alloc);
-		explicit vector(size_type count, const T& value = T(), const Allocator& alloc = Allocator());
-		template <class InputIt> vector(InputIt first, InputIt last, const Allocator& alloc = Allocator());
-		vector(const vector& other);
-
-	// iterator
-		iterator begin();
-		const_iterator begin() const;
-		iterator end();
-		const_iterator end() const;
-		reverse_iterator rbegin();
-		const_reverse_iterator rbegin() const;
-		reverse_iterator rend();
-		const_reverse_iterator rend() const;
-	};
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::vector() { }
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::vector(const Allocator& alloc)
-	: vector_base(alloc) { }
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::vector(size_type count, const T& value = T(), const Allocator& alloc = Allocator())
-	: vector_base(count, alloc) {
-		
+	inline void vector_base<T, Allocator>::create_storage(size_t count) {
+		this->begin = allocate(count);
+		this->end = this->begin;
+		this->end_cap = this->begin + this->count;
 	}
 
 	template <class T, class Allocator>
-	template <class InputIt>
-	inline vector<T, Allocator>::vector(InputIt first, InputIt last, const Allocator& alloc = Allocator()) {
-
+	inline vector_base<T, Allocator>::T_allocator_type&	vector_base<T, Allocator>::get_T_allocator() {
+		return (this->t_alloc);
 	}
 
 	template <class T, class Allocator>
-	inline vector<T, Allocator>::vector(const vector& other) {
-
+	inline const vector_base<T, Allocator>::T_allocator_type&	vector_base<T, Allocator>::get_T_allocator() const {
+		return (this->t_alloc);
 	}
 
 	template <class T, class Allocator>
-	inline vector<T, Allocator>::iterator vector<T, Allocator>::begin() {
-		return (iterator(this->begin));
-	}
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::const_iterator vector<T, Allocator>::begin() const {
-		return (const_iterator(this->begin));
-	}
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::iterator vector<T, Allocator>::end() {
-		return (iterator(this->end));
-	}
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::const_iterator vector<T, Allocator>::end() const {
-		return (const_iterator(this->end));
-	}
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::reverse_iterator vector<T, Allocator>::rbegin() {
-		return (reverse_iterator(this->end));
-	}
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::const_reverse_iterator vector<T, Allocator>::rbegin() const {
-		return (const_reverse_iterator(this->end));
-	}
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::reverse_iterator vector<T, Allocator>::rend() {
-		return (reverse_iterator(this->begin));
-	}
-
-	template <class T, class Allocator>
-	inline vector<T, Allocator>::const_reverse_iterator vector<T, Allocator>::rend() const {
-		return (const_reverse_iterator(this->begin));
+	inline vector_base<T, Allocator>::allocator_type	vector_base<T, Allocator>::get_allocator() const {
+		return (allocator_type(t_alloc));
 	}
 }
 #endif
