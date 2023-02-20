1659183690b1254e5b914620caaab4b0ebc8a659
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-02-21 01:08:52 +0900

    feat(mariadb): implement mariadb service

--- inception/srcs/requirements/mariadb/tools/usr/local/bin/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/usr/local/bin/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# SQL - SET PASSWORD for root to change its plugin into mysql_native_password is not recommended.
+# Debian assumes the use of unix_socket by default. 
+# Therefore, if you force the use of mysql_native_password, permission errors will occur when running commands such as 'service mysql start, status, stop.
+
 # start mysql daemon temporarily in background
 service mysql start
+
+# create database and user
+mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
 # improving security
 mysql_secure_installation << HEREDOC
@@ -15,10 +24,6 @@ y
 y
 HEREDOC
 
-mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
-mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
 # without sleep, service mysql stop prints messages like below.
 # [....] Stopping MariaDB database server: mysqld/usr/bin/mysqladmin: connect to server at 'localhost' failed
 # error: 'Can't connect to local MySQL server through socket '/var/run/mysqld/mysqld.sock' (2)'
@@ -28,4 +33,4 @@ sleep .5
 
 service mysql stop
 
-exec "$@"`
+exec "$@"
