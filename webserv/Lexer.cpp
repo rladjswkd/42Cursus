0cd2d7c89784b2ff79b6ba970ae552252d9c6c09
gyepark <49139312+rladjswkd@users.noreply.github.com>
2023-04-05 11:36:55 +0000

    feat: implement lexer to tokenize configuration files

---
+++ webserv/Lexer.cpp
@@ -0,0 +1,57 @@
+#include "Lexer.hpp"
+
+Lexer::Lexer(const char *filePath)
+{
+	this->configFile.open(filePath);
+}
+
+bool Lexer::isNotDelimiter(char c)
+{
+    return (WHITESPACES.find(c) == std::string::npos && BRACKET_SEMICOLON.find(c) == std::string::npos);
+}
+
+bool Lexer::isWhiteSpaces(char c)
+{
+    return (WHITESPACES.find(c) != std::string::npos);
+}
+
+bool Lexer::isBracketOrSemicolon(Delimiter delimiter)
+{
+	return (BRACKET_SEMICOLON.find(delimiter) != std::string::npos);
+}
+
+void Lexer::addToken(Tokens &tokens, Token token, Token delimiter)
+{
+	if (token.size())
+		tokens.push_back(token);
+	if (isBracketOrSemicolon(delimiter))
+		tokens.push_back(delimiter);
+}
+
+void Lexer::processToken(Tokens &tokens)
+{
+	Word			word;
+	std::filebuf	*fileBuf = configFile.rdbuf();
+	char			currentChar = fileBuf->sgetc();
+
+	while (isWhiteSpaces(currentChar))
+		currentChar = fileBuf->snextc();
+	while (currentChar != EOF && isNotDelimiter(currentChar))
+	{
+		word.push_back(currentChar);
+		currentChar = fileBuf->snextc();
+	}
+	addToken(tokens, word, Token(1, currentChar));
+	if (currentChar == EOF)
+		configFile.setstate(std::ios_base::eofbit);
+	fileBuf->sbumpc();
+}
+
+Lexer::Tokens Lexer::tokenize()
+{
+	Tokens	tokens;
+
+	while (!configFile.eof())
+		processToken(tokens);
+	return (tokens);
+}
---
+++ webserv/Lexer.hpp
@@ -0,0 +1,34 @@
+#ifndef LEXER_H
+# define LEXER_H
+
+# include <set>
+# include <vector>
+# include <string>
+# include <fstream>
+
+class Lexer
+{
+public:
+	typedef std::string			Token, Delimiter, Word;
+	typedef std::vector<Token>	Tokens;
+	typedef std::ifstream		ConfigFile;
+
+private:
+	ConfigFile	configFile;
+
+	Delimiter	WHITESPACES = " \n\t\r\f\v";
+	Delimiter	BRACKET_SEMICOLON = "{};";
+
+private:
+	void    addToken(Tokens &tokens, Token token, Token delimiter);
+	void	processToken(Tokens &tokens);
+	bool	isNotDelimiter(char c);
+	bool	isWhiteSpaces(char c);
+	bool	isBracketOrSemicolon(Delimiter delimiter);
+
+public:
+	Lexer(const char *filePath);
+	Tokens	tokenize();
+};
+
+#endif
